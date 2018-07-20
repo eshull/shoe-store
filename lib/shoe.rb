8f0ef166ce97c0057b009540d4a81c83ceddb265
Eric Shull <eshulldesign@gmail.com>
2018-07-20 15:01:16 -0700

    add places on site to display the shoes in store and stores that shoe is available in. Functionality is not working for display

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,9 @@
 class Shoe < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+
+  def where
+    # find name of store that store_id is on shoe instance
+    name = Store.find(:store_id)
+    name
+  end
 end
