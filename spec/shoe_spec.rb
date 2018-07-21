64627653829cee406a7bc940cf450b8dd3fea1d0
Eric Shull <eshulldesign@gmail.com>
2018-07-21 16:26:41 -0700

    add validation for presence of store name

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -2,4 +2,7 @@ require("spec_helper")
 
 describe(Shoe) do
   it { should have_and_belong_to_many(:stores) }
+  it { should have_db_column(:price) }
+  it { should validate_numericality_of(:price) }
+
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,5 +2,7 @@ require("spec_helper")
 
 describe(Store) do
   it { should have_and_belong_to_many(:shoes) }
+  it { should validate_presence_of(:name) }
   it { should validate_length_of(:name) }
+
 end
